fad6e3a0495d3f39bc67351d2f912b0c54f3bf0c
idm@thinkpad <isaias.dm.tech@gmail.com>
2025-11-20 09:21:37 +0000

    automated: sync dotfiles files for thinkpad

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = { enabled = false },
+      explorer = { enabled = false },
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+  },
+  {
+    "catppuccin/nvim",
+    enabled = false,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+}
